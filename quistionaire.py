2fcbd838010f11d7ead8feb7fc1f2fba70b03c94
Parthseh123 <86557891+Parthseh123@users.noreply.github.com>
2022-02-10 20:31:21 +0530

    Add files via upload

---
+++ quistionaire.py
@@ -0,0 +1,11 @@
+import random
+
+n = 0
+l =["Yes","No"]
+while n < 10:
+    a= input("Enter any question that can be answered in yes or no you want to ask from god - ")
+    an= random.choice(l)
+    print(an)
+
+
+
